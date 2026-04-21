63d9f1fc914981a993aad1dacdca24dda3c6331a
Md. Omar Faruk <omarfaruk.likhon@gmail.com>
2026-04-21 16:04:01 +0600

    feat: initialize DatabaseSeeder with essential application settings and administrative seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -24,14 +24,14 @@ class DatabaseSeeder extends Seeder
         $this->call(PlatformCommissionSettingSeeder::class);
         $this->call(PayoutThrseholdSeeder::class);
         $this->call(RefundPolicySettingSeeder::class);
-        
+
         // Create users
-        User::factory()->customer()->count(10)->create();
-        User::factory()->courier()->count(10)->create();
-        Order::factory()->count(9999)->create()->each(function($order) {
-            $order->order_number = 'LX-' . str_pad($order->id, 4, '0', STR_PAD_LEFT);
-            $order->saveQuietly(); // avoid triggering events again
-        });
-        
+        // User::factory()->customer()->count(10)->create();
+        // User::factory()->courier()->count(10)->create();
+        // Order::factory()->count(9999)->create()->each(function($order) {
+        //     $order->order_number = 'LX-' . str_pad($order->id, 4, '0', STR_PAD_LEFT);
+        //     $order->saveQuietly(); // avoid triggering events again
+        // });
+
     }
 }
